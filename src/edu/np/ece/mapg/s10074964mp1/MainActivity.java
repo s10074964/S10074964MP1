bd159204619043edc142f30f0723771199a8ac1c
s10074964 <s10074964@connect.np.edu.sg>
2014-11-26 12:14:57 +0800

    Initial Version
    1. Implemented required functions

--- src/edu/np/ece/mapg/s10074964mp1/MainActivity.java
+++ src/edu/np/ece/mapg/s10074964mp1/MainActivity.java
@@ -4,16 +4,51 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	
 	//initialize it to be the last digit of your student number
 	int myNumber = 4;
+	EditText etNumber;
+	Button btGuess;
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		etNumber = (EditText) this.findViewById(R.id.etNumber);
+		btGuess = (Button) this.findViewById(R.id.btGuess);
+		
+		btGuess.setOnClickListener(
+				new OnClickListener(){
+
+					@Override
+					public void onClick(View v) {
+						// TODO Auto-generated method stub
+						String str = etNumber.getText().toString();
+						int etNumber = Integer.parseInt(str);
+						
+						if(etNumber == myNumber){
+							Toast.makeText(MainActivity.this,
+									"Bingo! Correct Number.",
+									Toast.LENGTH_SHORT).show();
+						}else{
+							Toast.makeText(MainActivity.this,
+										"Try again!",
+										Toast.LENGTH_SHORT).show();
+						}
+					}}
+				
+			);
+			
+		
 	}
 
 	@Override
